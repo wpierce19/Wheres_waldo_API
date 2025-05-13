352281359f9aa91788697f0f3b96484ced0d753c
wpierce19 <126428398+wpierce19@users.noreply.github.com>
2025-05-13 14:58:24 -0600

    msc

--- controllers/scoreController.js
+++ controllers/scoreController.js
@@ -4,14 +4,16 @@ import asyncHandler from "express-async-handler";
 const prisma = new PrismaClient();
 
 const getScores = asyncHandler(async (req,res) => {
-    const scores = await prisma.scores.findMany({})
+    const scores = await prisma.scores.findMany({});
+    res.send(scores);
 });
 
 const saveScore = asyncHandler(async (req,res) => {
     const {username, time, clicks} = req.body;
     const data = {username: username, time: time, clicks: clicks};
     const newScore = await prisma.scores.create({data});
-    res.send(newScore);
+    //Commented out for now
+    //res.send(newScore);
 });
 
 export default {getScores, saveScore};
