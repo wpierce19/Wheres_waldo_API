1e051676753dbd92305f3d5d6255b4a457508b91
wpierce19 <126428398+wpierce19@users.noreply.github.com>
2025-05-16 15:26:05 -0600

    Update timerFunc.js

--- middlewares/timerFunc.js
+++ middlewares/timerFunc.js
@@ -1,31 +1,31 @@
 const timers = {};
 
-const startTimer = (userId, onTick) => {
+export const startTimer = (userId, onTick) => {
   if (timers[userId]) return;
 
   let currentTIme = 0;
+  timers[userId] = { interval: null, currentTIme };
+
   const interval = setInterval(() => {
     currentTIme++;
     timers[userId].currentTIme = currentTIme;
-    onTick(currentTIme);
+    onTick?.(currentTIme); // Optional callback, no network!
   }, 1000);
 
-  timers[userId] = {interval, currentTIme};
+  timers[userId].interval = interval;
 };
 
-const stopTimer = (userId) => {
-  if (timers[userId]) {
-    clearInterval(timers[userId].currentTIme);
-    const finalTime = timers[userId].currentTIme;
+export const stopTimer = (userId) => {
+  const timer = timers[userId];
+  if (timer) {
+    clearInterval(timer.interval);
+    const finalTime = timer.currentTIme;
     delete timers[userId];
     return finalTime;
   }
-
   return null;
 };
 
-const getTime = (userId) => {
+export const getTime = (userId) => {
   return timers[userId]?.currentTIme || 0;
-}
-
-export default {startTimer, stopTimer, getTime};
+};
