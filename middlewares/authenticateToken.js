20e6c418891cac1c4ec5d71fcda294d99f01410a
wpierce19 <126428398+wpierce19@users.noreply.github.com>
2025-05-16 15:12:07 -0600

    added debugging

--- middlewares/authenticateToken.js
+++ middlewares/authenticateToken.js
@@ -1,14 +1,25 @@
 import jwt from "jsonwebtoken";
 
-const authenticateJWT = (req,res,next) => {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
-    if (!token) return res.sendStatus(401);
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.user = user;
-        next();
-    });
+const authenticateJWT = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  console.log("🛡️ Auth Header:", authHeader);
+
+  if (!token) {
+    console.warn("⚠️ No token provided.");
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      console.warn("❌ Invalid token.");
+      return res.sendStatus(403);
+    }
+
+    req.user = user;
+    next();
+  });
 };
 
 export default authenticateJWT;
